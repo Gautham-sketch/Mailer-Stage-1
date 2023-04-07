81a1784cf8107582fc1f43cc2608381771bdf0df
Gautham-sketch <82305202+Gautham-sketch@users.noreply.github.com>
2023-04-07 19:03:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,3 +4,15 @@ const server = require("http").Server(app);
 app.use(express.json())
 
 server.listen(process.env.PORT || 3030);
+
+var nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  port : 465,
+  host : "smtp.gmail.com",
+  auth : {
+     user : 'gauthamnambiar172008india@gmail.com',
+     pass : 'worviienkyptgquk',
+  },
+  secure : true,
+})
